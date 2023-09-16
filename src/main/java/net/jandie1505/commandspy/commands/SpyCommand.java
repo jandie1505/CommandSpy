fbf219d867fce7d7ec74b930879c3aa08be0c439
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2023-09-16 15:02:27 +0200

    optimized imports

--- src/main/java/net/jandie1505/commandspy/commands/SpyCommand.java
+++ src/main/java/net/jandie1505/commandspy/commands/SpyCommand.java
@@ -8,9 +8,7 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 
